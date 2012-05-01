343c92dc6706103876b0298dee9ff3e3bfcdc640
Rene Schmidt <rene@reneschmidt.de>
2012-05-01 21:11:46 +0200

    refactored

--- BillomatPdfExporter.groovy
+++ BillomatPdfExporter.groovy
@@ -4,21 +4,42 @@ import groovyx.net.http.*
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
 
+/**
+ * Exports Billomat invoices as PDF
+ */
 class PdfExporter {
+
+    /**
+     * Invoice IDs
+     */
+    private ids = []
+
+    /**
+     * HTTPBuilder
+     */
+    private http
+
+    /**
+     * Constructor
+     */
+    PdfExporter() {
+        this.http = new HTTPBuilder('https://medefa.billomat.net')
+        this.http.setHeaders([
+                'User-Agent': 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4',
+                'X-BillomatApiKey': '4b28556815aeb8f67e3937524b73817d',
+                'Accept': 'application/json'
+        ])
+    }
 
     /**
      * Save PDF
      * @param invoiceId
      * @return
      */
-    def savePdf(invoiceId) {
-        def http = new HTTPBuilder('https://medefa.billomat.net')
-        http.request(GET, JSON) {
+    public void savePdf(invoiceId) {
+        this.http.request(GET, JSON) {
             uri.path = "/api/invoices/${invoiceId}/pdf"
             uri.query = ['format': 'json']
-
-            headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-            headers.'X-BillomatApiKey' = '4b28556815aeb8f67e3937524b73817d'
 
             response.success = { resp, json ->
                 def file = new File(json.document.filename)
@@ -31,44 +52,47 @@ class PdfExporter {
             }
         }
     }
-}
 
-/**
- * Get invoice ids
- * @param status
- * @return
- */
-def getInvoiceIds(status) {
-    def http = new HTTPBuilder('https://medefa.billomat.net')
-    http.request(GET, JSON) {
-        uri.path = '/api/invoices'
-        uri.query = ['status': status]
+    /**
+     * Get invoice ids
+     * @param status
+     * @return
+     */
+    public List getInvoiceIds(status) {
+        this.http.request(GET, JSON) {
+            uri.path = '/api/invoices'
+            uri.query = ['status': status]
 
-        headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-        headers.'X-BillomatApiKey' = '4b28556815aeb8f67e3937524b73817d'
-        headers.'Accept' = 'application/json'
+            response.success = { resp, json ->
+                ids = []
 
-        response.success = { resp, json ->
-            ids = [];
+                json.invoices.invoice.each { invoice ->
+                    ids.push(invoice.id)
+                }
 
-            json.invoices.invoice.each { invoice ->
-                ids.push(invoice.id)
+                return ids
             }
 
-            return ids
-        }
-
-        response.failure = { resp ->
-            println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+            response.failure = { resp ->
+                println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+            }
         }
     }
-}
 
+    /**
+     *
+     * @param arguments
+     */
+    static main(arguments) {
+        println "Starting export..."
 
-def billomat = new PdfExporter()
+        def exporter = new PdfExporter()
+        def invIds = exporter.getInvoiceIds('open')
 
-def invIds = getInvoiceIds('open')
+        invIds.each { invoiceId ->
+            exporter.savePdf(invoiceId)
+        }
 
-invIds.each { invoiceId ->
-    billomat.savePdf(invoiceId)
+        println "done."
+    }
 }
